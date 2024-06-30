6460e9d1c1284515d498509afb8c7771ca230569
Damon <damonmorris16@gmail.com>
2024-06-30 13:47:56 +0100

    challenge one

---
+++ challenge_one/hex-to-base64.go
@@ -0,0 +1,31 @@
+package challenge_one
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+func HexToBase64(hex string) (string, error) {
+	b, err := decodeHex(hex)
+	if err != nil {
+		fmt.Printf("failed to decode hex: %s", err)
+		return "", err
+	}
+	s := encodeBase64(b)
+	fmt.Printf("successfully encoded base64: %s", s)
+	return "", nil
+}
+
+func decodeHex(input string) ([]byte, error) {
+	b, err := hex.DecodeString(input)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func encodeBase64(input []byte) string {
+	s := base64.RawStdEncoding.EncodeToString(input)
+	return s
+}
